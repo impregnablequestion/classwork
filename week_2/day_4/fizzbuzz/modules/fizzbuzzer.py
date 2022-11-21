98b445bfa060949c9075933d6fac9ab1f6571ade
Séamus <ryans40@tcd.ie>
2022-11-21 13:49:49 +0000

    committed some bits

--- week_2/day_4/fizzbuzz/modules/fizzbuzzer.py
+++ week_2/day_4/fizzbuzz/modules/fizzbuzzer.py
@@ -4,8 +4,9 @@ def fizzbuzz(number):
 
     if divisible_by_3 and divisible_by_5:
         return "Fizzbuzz"
-    if divisible_by_3:
+    elif divisible_by_3:
         return "Fizz"
-    if divisible_by_5:
+    elif divisible_by_5:
         return "Buzz"
-    return f"{number}"
+    else:
+        return f"{number}"

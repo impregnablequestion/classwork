23a452899b84dd78d76636eba440008171eae705
Séamus <ryans40@tcd.ie>
2022-11-24 10:19:36 +0000

    made pizza objects

---
+++ week_3/day_4/pizzas/models/pizza.py
@@ -0,0 +1,7 @@
+class Pizza:
+    
+    def __init__(self, price, name, gluten_free, size):
+        self.price = price
+        self.name = name
+        self.gluten_free = gluten_free
+        self.size = size
---
+++ week_3/day_4/pizzas/models/pizzas.py
@@ -0,0 +1,8 @@
+from models.pizza import Pizza
+
+pizza_1 = Pizza(6.00, "Margherita", False, "M")
+pizza_2 = Pizza(7.00, "Pepperoni", False, "M")
+pizza_3 = Pizza(10.00, "Margherita", True, "L")
+pizza_4 = Pizza(5.00, "Pepperoni", False, "S")
+
+pizzas = [pizza_1, pizza_2, pizza_3, pizza_4]

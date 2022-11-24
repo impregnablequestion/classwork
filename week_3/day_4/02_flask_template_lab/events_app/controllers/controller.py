8905d69447164eca0ab4b40c4664769853eccb40
Séamus <ryans40@tcd.ie>
2022-11-24 17:12:41 +0000

    made it stylish in css

--- week_3/day_4/02_flask_template_lab/events_app/controllers/controller.py
+++ week_3/day_4/02_flask_template_lab/events_app/controllers/controller.py
@@ -1,6 +1,45 @@
-from flask import render_template
+from flask import render_template, redirect, request
 from app import app
+from models.event_list import event_list, get_event, add_event, delete_event
+from models.event import Event
+from datetime import date
+from datetime import datetime
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/events/')
+def events():
+    return render_template('events.html', events = event_list)
+
+@app.route('/events/<int:index>')
+def event(index):
+    event_id = get_event(index)
+    return render_template('event.html', event = event_id)
+
+@app.route('/events/add_event/')
+def new_event():
+    return render_template('eventplanner.html')
+
+@app.route('/events/', methods=['POST'])
+def submit_new_event():
+    form_data = request.form
+    uf_date = form_data["event_date"]
+    event_date = datetime.strptime(uf_date, "%Y-%m-%d")
+    event_name = form_data["event_name"]
+    event_number_of_guests = int(form_data["event_number_of_guests"])
+    event_location = form_data["event_location"]
+    event_description = form_data["event_description"]
+    event_recurring = True if "recurring" in request.form else False
+    
+    new_event = Event(
+        event_date, event_name, event_number_of_guests,
+        event_location, event_description, event_recurring)
+    add_event(new_event)
+    return redirect('/events/')
+
+@app.route('/events/delete/<name>')
+def remove_event(name):
+    delete_event(name)
+    return redirect('/events/')
--- week_3/day_4/02_flask_template_lab/events_app/models/event_list.py
+++ week_3/day_4/02_flask_template_lab/events_app/models/event_list.py
@@ -1,7 +1,24 @@
-from event import Event
+from models.event import Event
+from datetime import date
 
-event1 = Event((2022, 11, 28), "Events Research Programme", 50, "The Flying Duck", "Experimental mixed bill including some of Glasgow's best and strangest talent")
-event2 = Event((2022, 11, 26), "Stereo Bday Party", 300, "Stereo", "Anniversary party for famous city centre venue")
-event3 = Event((2022, 12, 20), "Fast Muzik", 275, "Stereo", "Christmas edition of popular happy hardcore/hyperpop night")
+
+event1 = Event(date(2022, 11, 28), "Events Research Programme", 50, "The Flying Duck", "Experimental mixed bill including some of Glasgow's best and strangest talent", True)
+event2 = Event(date(2022, 11, 26), "Stereo Bday Party", 300, "Stereo", "Anniversary party for famous city centre venue", False)
+event3 = Event(date(2022, 12, 20), "Fast Muzik", 275, "Stereo", "Christmas edition of popular happy hardcore/hyperpop night", True)
 
 event_list = [event1, event2, event3]
+
+def get_event(index):
+    return event_list[index]
+
+def add_event(event):
+    event_list.append(event)
+
+def delete_event(event):
+    for specific_event in event_list:
+        if specific_event.name == event:
+            event_to_delete = specific_event
+            break
+    event.remove(event_to_delete)
+
+    

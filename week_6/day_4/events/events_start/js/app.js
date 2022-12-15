ae02ac5bcbdf03ca92f417b8bf4c1880818b9b9a
Séamus <ryans40@tcd.ie>
2022-12-15 13:15:57 +0000

    added events lesson

---
+++ week_6/day_4/events/events_start/js/app.js
@@ -0,0 +1,8 @@
+// 1. which element will be dispatching the event?
+// 2. which event should we listen for?
+// 3. which piece of code should run when the event is dispatched?
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('JavaScript has loaded');
+});

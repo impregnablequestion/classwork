1e15cc573233f7fec84eaa812dd5ae780e42aae2
Séamus <ryans40@tcd.ie>
2022-12-15 13:12:49 +0000

    did dom intro

--- week_6/day_4/dom_intro/dom_intro_start/js/app.js
+++ week_6/day_4/dom_intro/dom_intro_start/js/app.js
@@ -1,1 +1,24 @@
+console.log("app loaded", window);
 
+document.addEventListener('DOMContentLoaded', () => {
+    const title = document.querySelector('h1');
+    title.textContent = 'Javascript says "Hello World"!';
+
+    const welcomeParagraph = document.querySelector('#welcome-paragraph');
+    console.log(welcomeParagraph);
+    console.dir(welcomeParagraph);
+
+    const redElement = document.querySelector('li.red');
+    console.dir(redElement);
+    redElement.textContent = 'I am a silly red element haha';
+    redElement.classList.add('bold');
+
+    const allListElements = document.querySelectorAll('li');
+    console.log(allListElements);
+    
+    const newListItem = document.createElement('li');
+    newListItem.textContent = 'Purple';
+    newListItem.classList.add('purple');
+    const list = document.querySelector('ul');
+    list.appendChild(newListItem);
+});

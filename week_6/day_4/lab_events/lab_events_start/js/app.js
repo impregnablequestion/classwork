e4560d72f802dfcbd998643c3597170a03834ba5
Séamus <ryans40@tcd.ie>
2022-12-21 11:27:51 +0000

    added dog api start

--- week_6/day_4/lab_events/lab_events_start/js/app.js
+++ week_6/day_4/lab_events/lab_events_start/js/app.js
@@ -1,5 +1,38 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript loaded');
 
-  
+  // const form
+
+  const handleForm = function (event) {
+    event.preventDefault();
+    const newList = document.createElement('ul');
+
+    const title = document.createElement('li');
+    title.textContent = `Title: ${event.target.title.value}`;
+    const author = document.createElement('li');
+    author.textContent = `Author: ${event.target.author.value}`; 
+    const category = document.createElement('li')
+    category.textContent = `Category: ${event.target.category.value}`;
+
+    newList.appendChild(title);
+    newList.appendChild(author);
+    newList.appendChild(category);
+    
+    const list = document.querySelector('#reading-list');
+    list.appendChild(newList);
+    
+    document.querySelector('#new-item-form').reset();
+  } 
+
+  const deleteAll = function () {
+    const list = document.querySelector('#reading-list');
+    list.replaceChildren();
+  }
+
+  const form = document.querySelector('#new-item-form');
+  form.addEventListener('submit', handleForm);
+
+  const button = document.querySelector('#delete-all');
+  button.addEventListener('click', deleteAll);
+
 })
---
+++ week_7/2_api_intro/dog_api_start/src/App.js
@@ -0,0 +1,22 @@
+import {useState} from "react";
+import './App.css';
+
+function App() {
+
+  const [dogImgUrl, setDogImgUrl] = useState("");
+
+  const fetchDog = function(){
+    const request = fetch("https://dog.ceo/api/breeds/image/random")
+    console.log(request);;
+  }
+
+  return (
+    <div id="app">
+    <h1>RANDOGMISER</h1>
+    <img id="dog-img" src={dogImgUrl} />
+    <button onClick={fetchDog}>Gimme those dogs!</button>
+    </div>
+  );
+}
+
+export default App;

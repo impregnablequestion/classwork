978c3cdc9e0446df99a747c4ad71ec4ca90b7569
Séamus <ryans40@tcd.ie>
2023-01-16 13:40:11 +0000

    tea biscuits lesson done

--- week_9/day_1/express_rest_api_teas_biscuits/express_rest_api_teas_biscuits_start/server/server.js
+++ week_9/day_1/express_rest_api_teas_biscuits/express_rest_api_teas_biscuits_start/server/server.js
@@ -10,8 +10,23 @@ const teas = [
   { name: "Green", brand: "Clipper" }
 ];
 
+const biscuits = [
+  { name: "Fig Rolls", brand: "Jacobs" },
+  { name: "Chocolate HobNobs", brand: "McVities"}
+];
+
 app.use(cors());
+app.use(express.json());
+
+const createRouter = require('./helpers/create_router.js')
+
+const teasRouter = createRouter(teas);
+app.use('/api/teas/', teasRouter);
+
+const biscuitsRouter = createRouter(biscuits);
+app.use('/api/biscuits/', biscuitsRouter);
 
 app.listen(9000, function () {
   console.log(`App running on port ${ this.address().port }`);
 });
+

d36ddaaa8821c2fe3c20d701aa415fe50036602b
Séamus <ryans40@tcd.ie>
2023-01-16 11:08:14 +0000

    finished intro to express

--- week_9/day_1/intro_to_express/intro_to_express_start/server/server.js
+++ week_9/day_1/intro_to_express/intro_to_express_start/server/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
+app.use(cors());
 
 app.listen(9000, function(){
    console.log('App running on port 9000'); 
 });
 
 app.get('/', function(req, res){
-    res.send('Hello World!');
+    res.json({message: "hiya kyle"})
 });

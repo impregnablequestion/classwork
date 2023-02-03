2e24189339d62507e4fe775c3fe3b3f963eae028
Séamus <ryans40@tcd.ie>
2023-02-03 14:54:52 +0000

    completed coding challenge

--- week_10/coding_challenges/phone-numbers-start/functions.js
+++ week_10/coding_challenges/phone-numbers-start/functions.js
@@ -1,6 +1,25 @@
+function createPhoneNumberForEach(numbers) {
+
+    let answer = "("
+    numbers.forEach((element, index) => {
+        if (index === 3) {
+            answer += ") " + element;
+        } else if (index === 6) {
+            answer += "-" + element;
+        } else {
+            answer += element
+        } 
+    });
+    return answer;
+}
+
+function createPhoneNumberTemp(numbers) {
+    return `(${numbers[0]}${numbers[1]}${numbers[2]}) ${numbers[3]}${numbers[4]}${numbers[5]}-${numbers[6]}${numbers[7]}${numbers[8]}${numbers[9]}`
+}
+
 function createPhoneNumber(numbers) {
-    // Please complete the function in order to satisfy the tests
-    return numbers;
+    [a, b, c, d, e, f, g, h, i, j] = numbers;
+    return `(${a}${b}${c}) ${d}${e}${f}-${g}${h}${i}${j}`
 }
 
 module.exports = createPhoneNumber;
